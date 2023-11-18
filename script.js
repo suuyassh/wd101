82b5e9fae3f5209d4f761f5dea92b423edb97f6c
suuyassh <151024539+suuyassh@users.noreply.github.com>
2023-11-18 12:05:41 +0530

    Update and rename index.js to script.js

---
+++ script.js
@@ -0,0 +1,95 @@
+if (localStorage.getItem("Names") == null) {
+    localStorage.setItem("Names", "[]");
+}
+if (localStorage.getItem("Emails") == null) {
+    localStorage.setItem("Emails", "[]");
+}
+if (localStorage.getItem("Passwords") == null) {
+    localStorage.setItem("Passwords", "[]");
+}
+if (localStorage.getItem("DOBs") == null) {
+    localStorage.setItem("DOBs", "[]");
+}
+
+const currentDate = new Date();
+const minDate = new Date();
+const maxDate = new Date();
+
+minDate.setFullYear(currentDate.getFullYear() - 55);
+
+maxDate.setFullYear(currentDate.getFullYear() - 18);
+ 
+
+document.getElementById("dob").setAttribute("min", minDate.toISOString().split('T')[0]);
+document.getElementById("dob").setAttribute("max", maxDate.toISOString().split('T')[0]);
+
+function displayData() {
+    const names = JSON.parse(localStorage.getItem("Names"));
+    const emails = JSON.parse(localStorage.getItem("Emails"));
+    const passwords = JSON.parse(localStorage.getItem("Passwords"));
+    const dobs = JSON.parse(localStorage.getItem("DOBs"));
+
+    const tableBody = document.querySelector("#datatable tbody");
+    
+    tableBody.innerHTML = "";
+
+    for (let i = 0; i < names.length; i++) {
+        let row = document.createElement("tr");
+        let nameCell = document.createElement("td");
+        let emailCell = document.createElement("td");
+        let passCell = document.createElement("td");
+        let dobCell = document.createElement("td");
+        let agreedCell = document.createElement("td");
+
+        nameCell.textContent = names[i];
+        emailCell.textContent = emails[i];
+        passCell.textContent = passwords[i];
+        dobCell.textContent = dobs[i];
+        agreedCell.textContent = "true";
+
+        row.appendChild(nameCell);
+        row.appendChild(emailCell);
+        row.appendChild(passCell);
+        row.appendChild(dobCell);
+        row.appendChild(agreedCell);
+
+        tableBody.appendChild(row);
+    }
+
+}
+
+displayData();
+
+function Submit() {
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+
+    var old_n = JSON.parse(localStorage.getItem("Names"));
+    old_n.push(name);
+    localStorage.setItem("Names", JSON.stringify(old_n));
+    
+    var old_e = JSON.parse(localStorage.getItem("Emails"));
+    old_e.push(email);
+    localStorage.setItem("Emails", JSON.stringify(old_e));
+    
+    var old_p = JSON.parse(localStorage.getItem("Passwords"));
+    old_p.push(password);
+    localStorage.setItem("Passwords", JSON.stringify(old_p));
+    
+    var old_d = JSON.parse(localStorage.getItem("DOBs"));
+    old_d.push(dob);
+    localStorage.setItem("DOBs", JSON.stringify(old_d));
+
+
+    
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("dob").value = "";
+
+    displayData();
+    return false;
+}
